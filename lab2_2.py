402b05874348ad1556b7f098048372bfe47fae58
kim <kim@kimui-MacBook-Pro.local>
2017-03-20 17:21:04 +0900

    lab2_2추가 -placeholder 사용 -예측 값 확

---
+++ lab2_2.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+X = tf.placeholder(tf.float32, shape=[None])
+Y = tf.placeholder(tf.float32, shape=[None])
+
+hypothesis = X * W + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(2001):
+    cost_val, W_val, b_val, _ = \
+        sess.run([cost, W, b, train],
+                 feed_dict={X: [1, 2, 3], Y: [2, 4, 6]})
+    if step % 20 == 0:
+        print(step, cost_val, W_val, b_val)
+
+print(sess.run(hypothesis, feed_dict={X: [5]}))
+print(sess.run(hypothesis, feed_dict={X: [2.5]}))
+print(sess.run(hypothesis, feed_dict={X: [1.5, 3.5]}))
